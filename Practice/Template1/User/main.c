83db65310ce79209556ffc28df27fec3684f6ac9
tang <tangzh@inhand.com.cn>
2021-07-23 19:58:16 +0800

    互斥锁

--- Practice/Template1/User/main.c
+++ Practice/Template1/User/main.c
@@ -35,8 +35,9 @@ static rt_thread_t led1_thread = RT_NULL;
 static rt_thread_t led2_thread = RT_NULL;
 static rt_thread_t key0_thread = RT_NULL;
 static rt_thread_t key1_thread = RT_NULL;
-/* 定义信号量控制块 */
-static rt_sem_t test_sem = RT_NULL;
+/* 定义互斥量控制块 */
+static rt_mutex_t test_mux = RT_NULL;
+uint32_t num = 0;
 /*
 *************************************************************************
 *                             函数声明
@@ -73,45 +74,35 @@ static void led2_thread_entry(void *parameter)
 
 static void key0_thread_entry(void *parameter)
 {
-    rt_err_t uwRet = RT_EOK;
-
     while (1)
     {
-        if (Key_Scan(KEY1_GPIO_PORT, KEY1_GPIO_PIN) == KEY_ON)/* K1 被按下 */
-        {
-            uwRet = rt_sem_release(test_sem);     //释放信号量
-            if (RT_EOK != uwRet)
-            {
-                rt_kprintf("release failed:%lx\n", uwRet);
-            }
-            else
-            {
-                rt_kprintf("release one sem\n");
-            }
-        }
+        rt_mutex_take(test_mux,           /* 获取互斥量 */
+                      RT_WAITING_FOREVER);    /* 等待时间：一直等 */
+        ++num;
+        rt_thread_delay(1000);
+        rt_thread_delay(1000);
+        rt_thread_delay(1000);
 
-        rt_thread_delay(20);
+        rt_mutex_release(test_mux);    //释放互斥量
+
+        rt_thread_delay(10);
+
     }
-
 
 }
 
 static void key1_thread_entry(void *parameter)
 {
-    rt_err_t uwRet = RT_EOK;
     while (1)
     {
-        if (Key_Scan(KEY2_GPIO_PORT, KEY2_GPIO_PIN) == KEY_ON)       //如果KEY2被单击
-        {
-            /* 获取一个计数信号量 */
-            uwRet = rt_sem_take(test_sem, 0);  /* 获取信号量 */
-            if (RT_EOK == uwRet)
-                rt_kprintf("take one sem\r\n");
-            else
-                rt_kprintf("up\r\n");
+        rt_mutex_take(test_mux,           /* 获取互斥量 */
+                      RT_WAITING_FOREVER);    /* 等待时间：一直等 */
 
-        }
-        rt_thread_delay(20);     //每20ms扫描一次
+        rt_kprintf("num:%d\n", num);
+
+        rt_mutex_release(test_mux);    //释放互斥量
+
+        rt_thread_delay(10);
 
     }
 
@@ -134,7 +125,7 @@ int main(void)
                          led1_thread_entry,   /* 线程入口函数 */
                          RT_NULL,             /* 线程入口函数参数 */
                          512,                 /* 线程栈大小 */
-                         3,                   /* 线程的优先级 */
+                         5,                   /* 线程的优先级 */
                          20);                 /* 线程时间片 */
 
     /* 启动线程，开启调度 */
@@ -156,13 +147,14 @@ int main(void)
         rt_thread_startup(led2_thread);
     else
         return -1;
-
-    /* 创建一个信号量 */
-    test_sem = rt_sem_create("test_sem",/* 信号量名字 */
-                             5,     /* 信号量初始值 */
-                             RT_IPC_FLAG_FIFO); /* 信号量模式 FIFO(0x00)*/
-    if (test_sem != RT_NULL)
-        rt_kprintf("create sem success\n\n");
+    /* 
+    使用 RT_IPC_FLAG_PRIO 优先级 flag 创建的 IPC 对象，在多个线程等
+    待资源时，将由优先级高的线程优先获得资源。而使用 RT_IPC_FLAG_FIFO 先进先出 flag
+    创建的 IPC 对象，在多个线程等待资源时，将按照先来先得的顺序获得资源。 
+    */
+    test_mux = rt_mutex_create("test_mux", RT_IPC_FLAG_PRIO);
+    if (test_mux != RT_NULL)
+        rt_kprintf("creat mux success\n\n");
 
     key1_thread =                          /* 线程控制块指针 */
         rt_thread_create("key1",               /* 线程名字 */
